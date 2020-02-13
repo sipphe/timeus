6b5fe7da2f2fade3339e05a09dc1133b5166fdf2
sipphe <kambude@gmail.com>
2020-02-14 00:26:10 +0200

    release 1.0.0
    Signed-off-by: sipphe <kambude@gmail.com>

--- app/src/main/java/com/timeus/MainActivity.kt
+++ app/src/main/java/com/timeus/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.timeus
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.os.Handler
 import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import java.time.Duration
 import java.time.LocalDateTime
 import java.time.Month
@@ -18,6 +22,9 @@ class MainActivity : AppCompatActivity() {
     var textMinutes: TextView? = null
     var textSeconds: TextView? = null
     var duration: LocalDateTime? = null
+    var imageSlideShow: ImageView? = null
+    var layoutSpotify: ConstraintLayout? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -28,20 +35,64 @@ class MainActivity : AppCompatActivity() {
         textHours = findViewById(R.id.textHours)
         textMinutes = findViewById(R.id.textMinutes)
         textSeconds = findViewById(R.id.textSeconds)
+        imageSlideShow = findViewById(R.id.imageViewSlideshow)
+        layoutSpotify = findViewById(R.id.layoutSpotify)
 
         setTexts()
         val handler = Handler()
-        object : Runnable {
-            override fun run() {
 
+        handler.post(object : Runnable {
+            override fun run() {
                 setTexts()
                 handler.postDelayed(this, 1000)
             }
+        })
+
+        val handler2 = Handler()
+        var count = 0
+        handler2.post(object : Runnable {
+            override fun run() {
+                when (count) {
+                    5 -> {
+                        imageSlideShow!!.setImageDrawable(resources.getDrawable(R.drawable.i_m_0))
+                        count = 0
+                    }
+                    0 -> {
+                        imageSlideShow!!.setImageDrawable(resources.getDrawable(R.drawable.i_m_1))
+                        count = 1
+                    }
+                    1 -> {
+                        imageSlideShow!!.setImageDrawable(resources.getDrawable(R.drawable.i_m_2))
+                        count = 2
+                    }
+                    2 -> {
+                        imageSlideShow!!.setImageDrawable(resources.getDrawable(R.drawable.i_m_3))
+                        count = 3
+                    }
+                    3 -> {
+                        imageSlideShow!!.setImageDrawable(resources.getDrawable(R.drawable.i_m_4))
+                        count = 4
+                    }
+                    4 -> {
+                        imageSlideShow!!.setImageDrawable(resources.getDrawable(R.drawable.i_m_5))
+                        count = 5
+                    }
+                }
+                handler.postDelayed(this, 6580)
+            }
+        })
+
+        layoutSpotify!!.setOnClickListener {
+            val url = "https://open.spotify.com/playlist/7elvgH5i5P69YcqvUHW1Xq"
+
+            val i = Intent(Intent.ACTION_VIEW)
+            i.data = Uri.parse(url)
+            startActivity(i)
         }
     }
 
     private fun setTexts() {
-        duration = getCounterFromDate(LocalDateTime.of(2019, Month.SEPTEMBER, 28, 20, 0, 0, 0))
+        duration = getCounterFromDate(LocalDateTime.of(2019, Month.SEPTEMBER, 28, 19, 30, 0, 0))
 
         val years = duration!!.year.toString()
         if (years == "0") {
